530cec1a522479f17351a8f75be2604db86fd225
1ulisses <leandrocdefarias@proton.me>
2026-08-27 10:06:57 -0300

    feat: initial files

---
+++ notebook/01_data_exploration.py
@@ -0,0 +1,9 @@
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# %%
+df_book = pd.read_csv('../data/raw/Books.csv')
+df_ratings = pd.read_csv('../data/raw/Ratings.csv')
+df_users = pd.read_csv('../data/raw/Users.csv')
